a3630b32147b7b1ea0f8ad55e485447c16461a8e
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-17 13:10:52 -0500

    Initial Commit

---
+++ PracticaBloque3Tests/PracticaBloque3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticaBloque3Tests.swift
+//  PracticaBloque3Tests
+//
+//  Created by Gopenux on 17/06/25.
+//
+
+import Testing
+@testable import PracticaBloque3
+
+struct PracticaBloque3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
