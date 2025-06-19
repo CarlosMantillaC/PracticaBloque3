9b385ecc925d62231d180a78646b6be8586aa944
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-19 13:14:42 -0500

    feat: add ThemeManager

---
+++ PracticaBloque3/Model/ThemeManager.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeManager.swift
+//  PracticaBloque3
+//
+//  Created by Gopenux on 19/06/25.
+//
+
+import Foundation
+
+final class ThemeManager {
+    static let shared = ThemeManager()
+    private init() {}
+
+    private(set) var currentTheme: String = "light"
+
+    func toggleTheme() {
+        currentTheme = currentTheme == "light" ? "dark" : "light"
+        NotificationCenter.default.post(name: .themeChanged, object: nil, userInfo: ["theme": currentTheme])
+    }
+}
